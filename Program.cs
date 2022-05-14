e97c6a49b806bae0dd66895c418d894905bd95fc
webpagify <jdipierro@live.com>
2022-05-13 21:50:29 -0700

    Requirements #4 - Deny negative numbers by throwing an exception that includes all of the negative numbers provided

--- Program.cs
+++ Program.cs
@@ -39,6 +39,10 @@ namespace calculatorChallenge
                 // added for requirement #3
                 testCalculator.TestAdd_NewLineDelimitedInput_ReturnsInputSum();
 
+                // added for requirement #4
+                testCalculator.TestAdd_NegativeNumber_ThrowsException();
+                testCalculator.TestAdd_NegativeNumbers_ThrowsException_Message();
+
                 Console.WriteLine("All Test Cases Passed");
             }
             catch (Exception e)
@@ -52,17 +56,22 @@ namespace calculatorChallenge
 
     static class Calculator
     {
+        public const string NegativeNumberErrorMessage = "Input contains negative numbers:  ";
+
         /// <summary>
-        /// Support a maximum of 2 numbers using a comma delimiter. Throw an exception when more than 2 numbers are provided
-        /// examples: 20 will return 20; 1,5000 will return 5001; 4,-3 will return 1
+        /// Support a maximum of 2 numbers using a comma or newline delimiter. 
+        /// examples: 20 will return 20; 1,5000 will return 5001,
+        ///      1,2,3,4,5,6,7,8,9,10,11,12 will return 78,  1\n2,3 will return 6
         /// empty input or missing numbers should be converted to 0
         /// invalid numbers should be converted to 0 e.g. 5,tytyt will return 5
+        /// negative numbers will throw an exception that includes all of the negative numbers provided
         /// NOTE: whitespace between the input values will be ignored
         /// </summary>
         /// <param name="input"></param>
         /// <returns>the sum of the delimited input values</returns>
         public static int Add(string input)
         {
+
             if (String.IsNullOrWhiteSpace(input))
             {
                 return 0;
@@ -71,23 +80,27 @@ namespace calculatorChallenge
             int returnValue = 0;
             char[] delimiter = { ',', '\n' };
             string[] splitInput = input.Split(delimiter);
+            string negativeNumbers = String.Empty;
 
-            if (splitInput.Count() == 1)
+            // 1 or more input values exist
+            foreach (var splitInputString in splitInput)
             {
-                int.TryParse(splitInput[0], out returnValue);
-            }
-            else
-            {
-                // 2 or more input values exist
-                foreach (var splitInputString in splitInput)
+                int splitInputValue = 0;
+
+                if (int.TryParse(splitInputString, out splitInputValue))
                 {
-                    int splitInputValue = 0;
-
-                    if (int.TryParse(splitInputString, out splitInputValue))
+                    if (splitInputValue < 0)
                     {
-                        returnValue += splitInputValue;
+                        negativeNumbers += String.IsNullOrEmpty(negativeNumbers) ? splitInputValue.ToString() : ", " + splitInputValue.ToString();
                     }
+
+                    returnValue += splitInputValue;
                 }
+            }
+
+            if (!String.IsNullOrEmpty(negativeNumbers))
+            {
+                throw new ArgumentException(NegativeNumberErrorMessage + negativeNumbers);
             }
 
             return returnValue;
@@ -194,6 +207,48 @@ namespace calculatorChallenge
             Assert.AreEqual(actualResult, expectedResult);
         }
 
-    }
+        [TestMethod()]
+        public void TestAdd_NegativeNumber_ThrowsException()
+        {
+            //arrange
+            string input = "1,-2,3";
+            // expected result = an Argument Exception exception will be thrown with a message that will contain the negative number provided
 
+            //act + assert
+            Assert.ThrowsException<ArgumentException>(() => Calculator.Add(input));
+        }
+
+        [TestMethod()]
+        public void TestAdd_NegativeNumbers_ThrowsException()
+        {
+            //arrange
+            string input = "1,-2,3,40,-42,-53,67";
+            // expected result = an Argument Exception exception will be thrown with a message that lists all of the negative numbers provided
+
+            //act + assert
+            Assert.ThrowsException<ArgumentException>(() => Calculator.Add(input));
+        }
+
+        [TestMethod()]
+        public void TestAdd_NegativeNumbers_ThrowsException_Message()
+        {
+            //arrange
+            string input = "1,-2,3,40,-42,-53,67";
+            string expectedErrorMessage = Calculator.NegativeNumberErrorMessage + "-2, -42, -53";
+            string actualErrorMessage = String.Empty;
+
+            //act
+            try
+            {
+                int actualResult = Calculator.Add(input);
+            }
+            catch (Exception e)
+            {
+                actualErrorMessage = e.Message;
+            }
+            
+            //assert
+            Assert.AreEqual(actualErrorMessage, expectedErrorMessage);
+        }
+    }
 }
